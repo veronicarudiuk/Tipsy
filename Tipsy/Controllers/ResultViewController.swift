44bb39c7ef62993147432a8c0e9af8e92a239b76
Veronica Rudiuk <veronicarudiuk@gmail.com>
2022-11-01 13:55:02 +0400

    Create IBActions and IBOutlets for the Results Screen

---
+++ Tipsy/Controllers/ResultViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Veronica Rudiuk on 1.11.22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var settingsLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func recalculatePressed(_ sender: Any) {
+    }
+    
+}
